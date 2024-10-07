1d35d4e736c4078403c9ae5e39d5984e0028fc43
Reigo Kurgpõld <114922120+reigoku@users.noreply.github.com>
2024-10-07 03:50:45 +0300

    feat: add theme to main page
    added a theme to the main page using dark theme and a cyan-ish main color

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,29 @@
+
 import "./App.css";
 import Cats from "./components/Cats";
 import Todos from "./components/Todo";
+import {createTheme, ThemeProvider} from "@mui/material/styles";
+import { purple } from '@mui/material/colors';
+import { CssBaseline } from "@mui/material";
+
+const outerTheme = createTheme({
+  palette: {
+    mode: 'dark', //the best theme.
+    primary: {
+      main: '#3fb5a4',
+    },
+  },
+});
+
 
 function App() {
   return (
     <>
-      <div>
+      <ThemeProvider theme={outerTheme}>
+        <CssBaseline />
         <Cats />
         <Todos />
-      </div>
+      </ThemeProvider>
     </>
   );
 }
